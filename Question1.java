56797803511384790546b5825b0f8914b2dd3c8a
nandhu06prabha <153995777+nandhu06prabha@users.noreply.github.com>
2023-12-21 11:47:16 +0530

    Create Question1.java

---
+++ Question1.java
@@ -0,0 +1,16 @@
+package task1;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner obj = new Scanner(System.in);
+		int userInput = obj.nextInt();
+		for (int i = 10; i <= 50; i++) {
+			System.out.println(i);
+		}
+	}
+	
+	
+}
